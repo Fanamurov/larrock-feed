5ca22bf6ab7ef271fddd6e77d4ce73ef1dc5cd0f
Alexandr Fanamurov <fanamura@gmail.com>
2017-07-06 13:30:13 +1000

    Fix

--- src/routes.php
+++ src/routes.php
@@ -20,6 +20,6 @@ Route::group(['middleware' => $middlewares], function(){
     ]);
 });
 
-Route::group(['prefix' => 'admin', 'middleware'=> ['web', 'level:2', 'LarrockAdminMenu']], function(){
+Route::group(['prefix' => 'admin', 'middleware'=> ['web', 'level:2', 'LarrockAdminMenu', 'SaveAdminPluginsData']], function(){
     Route::resource('feed', AdminFeedController::class);
 });
