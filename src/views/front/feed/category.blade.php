fb2e1a6117c4918ed45489660cb23f07eeb372af
Alexandr Fanamurov <fanamura@gmail.com>
2017-10-24 16:33:47 +1000

    update template

--- src/views/front/feed/category.blade.php
+++ src/views/front/feed/category.blade.php
@@ -18,9 +18,21 @@
                 <div class="uk-width-1-1 uk-width-medium-2-10">
                     <p class="date uk-text-muted uk-text-right">{!! \Carbon\Carbon::parse($item->date)->format('d.m.Y') !!}г.</p>
                 </div>
-                <div class="uk-width-1-1 uk-width-medium-8-10 uk-position-relative">
+                <div class="uk-width-1-1 uk-width-medium-8-10">
                     @role('Админ|Модератор')
                     <a class="admin_edit" href="/admin/feed/{{ $item->id }}/edit">Редактировать</a>
+                    @endrole
+                    <h3 class="uk-margin-top-remove"><a href="{{ $item->full_url }}">{{ $item->title }}</a></h3>
+                    <div class="pageFeedCategory-item_short">{!! $item->short !!}</div>
+                </div>
+            </div>
+        @endforeach
+
+        @foreach($data->get_child as $item)
+            <div class="pageFeedCategory-item uk-grid">
+                <div class="uk-width-1-1">
+                    @role('Админ|Модератор')
+                    <a class="admin_edit" href="/admin/category/{{ $item->id }}/edit">Редактировать</a>
                     @endrole
                     <h3 class="uk-margin-top-remove"><a href="{{ $item->full_url }}">{{ $item->title }}</a></h3>
                     <div class="pageFeedCategory-item_short">{!! $item->short !!}</div>
