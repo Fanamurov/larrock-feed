e05ed3e7437e71de43930b007958ce052ca54d83
Alexandr Fanamurov <fanamura@gmail.com>
2017-10-27 13:27:22 +1000

    add GetFilesAndImages and GetSeo Traits

--- src/Models/Feed.php
+++ src/Models/Feed.php
@@ -6,16 +6,16 @@ use Cache;
 use Larrock\Core\Helpers\Plugins\RenderPlugins;
 use Illuminate\Database\Eloquent\Model;
 use Larrock\ComponentCategory\Facades\LarrockCategory;
-use Larrock\Core\Models\Seo;
+use Larrock\Core\Traits\GetFilesAndImages;
+use Larrock\Core\Traits\GetSeo;
 use Nicolaslopezj\Searchable\SearchableTrait;
 use DB;
 use Spatie\MediaLibrary\HasMedia\HasMediaTrait;
 use Spatie\MediaLibrary\HasMedia\Interfaces\HasMediaConversions;
 use Larrock\ComponentFeed\Facades\LarrockFeed;
-use Spatie\MediaLibrary\Media;
 
 /**
- * App\Models\Feed
+ * Larrock\ComponentFeed\Models\Feed
  *
  * @property integer $id
  * @property string $title
@@ -28,57 +28,44 @@ use Spatie\MediaLibrary\Media;
  * @property integer $active
  * @property \Carbon\Carbon $created_at
  * @property \Carbon\Carbon $updated_at
- * @method static \Illuminate\Database\Query\Builder|\App\Models\Feed whereId($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Models\Feed whereTitle($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Models\Feed whereCategory($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Models\Feed whereShort($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Models\Feed whereDescription($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Models\Feed whereUrl($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Models\Feed whereDate($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Models\Feed wherePosition($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Models\Feed whereActive($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Models\Feed whereCreatedAt($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Models\Feed whereUpdatedAt($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Models\Feed find($value)
- * @method static \Illuminate\Database\Query\Builder|\App\Models\Feed categoryInfo()
+ * @method static \Illuminate\Database\Query\Builder|\Larrock\ComponentFeed\Models\Feed whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\Larrock\ComponentFeed\Models\Feed whereTitle($value)
+ * @method static \Illuminate\Database\Query\Builder|\Larrock\ComponentFeed\Models\Feed whereCategory($value)
+ * @method static \Illuminate\Database\Query\Builder|\Larrock\ComponentFeed\Models\Feed whereShort($value)
+ * @method static \Illuminate\Database\Query\Builder|\Larrock\ComponentFeed\Models\Feed whereDescription($value)
+ * @method static \Illuminate\Database\Query\Builder|\Larrock\ComponentFeed\Models\Feed whereUrl($value)
+ * @method static \Illuminate\Database\Query\Builder|\Larrock\ComponentFeed\Models\Feed whereDate($value)
+ * @method static \Illuminate\Database\Query\Builder|\Larrock\ComponentFeed\Models\Feed wherePosition($value)
+ * @method static \Illuminate\Database\Query\Builder|\Larrock\ComponentFeed\Models\Feed whereActive($value)
+ * @method static \Illuminate\Database\Query\Builder|\Larrock\ComponentFeed\Models\Feed whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\Larrock\ComponentFeed\Models\Feed whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\Larrock\ComponentFeed\Models\Feed find($value)
+ * @method static \Illuminate\Database\Query\Builder|\Larrock\ComponentFeed\Models\Feed categoryInfo()
  * @property-read \Illuminate\Database\Eloquent\Collection|\Spatie\MediaLibrary\Media[] $media
  * @mixin \Eloquent
  * @property integer $user_id
  * @property-read mixed $first_image
  * @property-read mixed $full_url
- * @method static \Illuminate\Database\Query\Builder|\App\Models\Feed whereUserId($value)
+ * @method static \Illuminate\Database\Query\Builder|\Larrock\ComponentFeed\Models\Feed whereUserId($value)
  * @property-read mixed $get_seo_title
- * @method static \Illuminate\Database\Query\Builder|\App\Models\Feed search($search, $threshold = null, $entireText = false, $entireTextOnly = false)
- * @method static \Illuminate\Database\Query\Builder|\App\Models\Feed searchRestricted($search, $restriction, $threshold = null, $entireText = false, $entireTextOnly = false)
+ * @method static \Illuminate\Database\Query\Builder|\Larrock\ComponentFeed\Models\Feed search($search, $threshold = null, $entireText = false, $entireTextOnly = false)
+ * @method static \Illuminate\Database\Query\Builder|\Larrock\ComponentFeed\Models\Feed searchRestricted($search, $restriction, $threshold = null, $entireText = false, $entireTextOnly = false)
  */
 class Feed extends Model implements HasMediaConversions
 {
     use HasMediaTrait;
+    use GetFilesAndImages;
+    use GetSeo;
+    use SearchableTrait;
 
     public function __construct(array $attributes = [])
     {
         parent::__construct($attributes);
         $this->fillable(LarrockFeed::addFillableUserRows(['title', 'short', 'description', 'category', 'url', 'date', 'position', 'active']));
         $this->table = LarrockFeed::getConfig()->table;
+        $this->modelName = LarrockFeed::getModelName();
+        $this->componentName = 'feed';
     }
-
-    public function registerMediaConversions(Media $media = null)
-    {
-        $this->addMediaConversion('110x110')
-            ->height(110)->width(110)
-            ->performOnCollections('images');
-
-        $this->addMediaConversion('140x140')
-            ->height(140)->width(140)
-            ->performOnCollections('images');
-
-        $this->addMediaConversion('250x250')
-            ->height(250)->width(250)
-            ->performOnCollections('images');
-    }
-    
-
-    use SearchableTrait;
 
     // no need for this, but you can define default searchable columns:
     protected $searchable = [
@@ -110,35 +97,6 @@ class Feed extends Model implements HasMediaConversions
         return $this->hasOne(LarrockCategory::getModelName(), 'id', 'category');
     }
 
-    public function get_seo()
-    {
-        return $this->hasOne(Seo::class, 'seo_id_connect', 'id')->whereSeoTypeConnect('feed');
-    }
-
-    public function getImages()
-    {
-        return $this->hasMany('Spatie\MediaLibrary\Media', 'model_id', 'id')->where([['model_type', '=', LarrockFeed::getModelName()], ['collection_name', '=', 'images']])->orderBy('order_column', 'DESC');
-    }
-    public function getFirstImage()
-    {
-        return $this->hasOne('Spatie\MediaLibrary\Media', 'model_id', 'id')->where([['model_type', '=', LarrockFeed::getModelName()], ['collection_name', '=', 'images']])->orderBy('order_column', 'DESC');
-    }
-
-    public function getFiles()
-    {
-        return $this->hasMany('Spatie\MediaLibrary\Media', 'model_id', 'id')->where([['model_type', '=', LarrockFeed::getModelName()], ['collection_name', '=', 'files']])->orderBy('order_column', 'DESC');
-    }
-
-    public function getFirstImageAttribute()
-    {
-        return Cache::remember('image_f_feed'. $this->id, 1440, function() {
-            if($get_image = $this->getMedia('images')->sortByDesc('order_column')->first()){
-                return $get_image->getUrl();
-            }
-            return '/_assets/_front/_images/empty_big.png';
-        });
-    }
-
     public function getFullUrlAttribute()
     {
         return Cache::remember('url_feed'. $this->id, 1440, function() {
@@ -148,23 +106,6 @@ class Feed extends Model implements HasMediaConversions
             }
             $url .= '/'. $this->url;
             return $url;
-        });
-    }
-
-    public function getGetSeoTitleAttribute()
-    {
-        return Cache::remember('seo_title'. $this->id .'_'. $this->url, 1440, function() {
-            if($get_seo = Seo::whereSeoIdConnect($this->id)->first()){
-                if($get_seo->seo_title){
-                    return $get_seo->seo_title;
-                }
-            }
-            if($get_seo = Seo::whereSeoUrlConnect($this->url)->first()){
-                if($get_seo->seo_title){
-                    return $get_seo->seo_title;
-                }
-            }
-            return NULL;
         });
     }
 
